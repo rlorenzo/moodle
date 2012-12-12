9e8e3270c2eb761d7ba8ab765603deda5f0b3f8d
Rex Lorenzo <rex@oid.ucla.edu>
2012-12-12 15:47:23 -0800

    MDL-32485 - Course content inaccessible when groupmembersonly is enabled, but no grouping is set
    * Changed module form to disable "Available for group members only" if "None" grouping is chosen

--- course/moodleform_mod.php
+++ course/moodleform_mod.php
@@ -179,16 +179,17 @@ abstract class moodleform_mod extends moodleform {
         if (empty($this->_cm) || !$this->_cm->groupingid) {
             if ($mform->elementExists('groupmode') and !$mform->elementExists('groupmembersonly') and empty($COURSE->groupmodeforce)) {
                 $mform->disabledIf('groupingid', 'groupmode', 'eq', NOGROUPS);
-
-            } else if (!$mform->elementExists('groupmode') and $mform->elementExists('groupmembersonly')) {
-                $mform->disabledIf('groupingid', 'groupmembersonly', 'notchecked');
-
             } else if (!$mform->elementExists('groupmode') and !$mform->elementExists('groupmembersonly')) {
                 // groupings have no use without groupmode or groupmembersonly
                 if ($mform->elementExists('groupingid')) {
                     $mform->removeElement('groupingid');
                 }
             }
+        }
+
+        // disable groupmembersonly checkbox if "None" grouping is choosen
+        if ($CFG->enablegroupmembersonly) {
+            $mform->disabledIf('groupmembersonly', 'groupingid', 'eq', 0);
         }
 
         // Completion: If necessary, freeze fields
